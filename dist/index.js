3c8014b94c08eb31abaa9c6e325259d74466d875
James Coward <jcoward@elsevier.com>
2017-10-24 17:49:25 +0100

    simplifed babel index

--- dist/index.js
+++ dist/index.js
@@ -11,33 +11,46 @@ var _resolver2 = _interopRequireDefault(_resolver);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = ({ types: t }) => {
-  const importVisitors = {
-    ImportDeclaration(path, state) {
-      const lib = state.opts.lib;
-      const libNode = path.node.source;
-      if (!(t.isStringLiteral(libNode) && libNode.value === lib)) {
-        return;
-      }
-      path.get('specifiers').forEach(spec => {
-        const localName = spec.get('local').node.name;
-        const name = spec.get('imported').node.name;
-        const identifier = t.identifier(localName);
-        const importDefaultSpecifier = t.importDefaultSpecifier(identifier);
-        const pathToImport = (0, _resolver2.default)(lib, name);
-        const importDeclaration = t.importDeclaration([importDefaultSpecifier], t.stringLiteral(pathToImport));
-        path.insertBefore(importDeclaration);
-      });
+  // const importVisitors = {
+  //   ImportDeclaration(path, state) {
+  //     const lib = state.opts.lib;
+  //     const libNode = path.node.source
+  //     if(!(t.isStringLiteral(libNode) && libNode.value === lib)) {
+  //       return;
+  //     }
+  //     path.get('specifiers').forEach((spec) => {
+  //       const localName = spec.get('local').node.name;
+  //       const name = spec.get('imported').node.name;
+  //       const identifier = t.identifier(localName);
+  //       const importDefaultSpecifier = t.importDefaultSpecifier(identifier);
+  //       const pathToImport = resolver(lib, name);
+  //       const importDeclaration = t.importDeclaration([importDefaultSpecifier], t.stringLiteral(pathToImport));
+  //       path.insertBefore(importDeclaration);
+  //     });
 
-      path.remove();
-    }
-  };
+  //     path.remove();
+  //   },
+  // };
 
   return {
     visitor: {
-      Program: {
-        exit(programPath, state) {
-          programPath.traverse(importVisitors, state);
+      ImportDeclaration(path, state) {
+        const lib = state.opts.lib;
+        const libNode = path.node.source;
+        if (!(t.isStringLiteral(libNode) && libNode.value === lib)) {
+          return;
         }
+        path.get('specifiers').forEach(spec => {
+          const localName = spec.get('local').node.name;
+          const name = spec.get('imported').node.name;
+          const identifier = t.identifier(localName);
+          const importDefaultSpecifier = t.importDefaultSpecifier(identifier);
+          const pathToImport = (0, _resolver2.default)(lib, name);
+          const importDeclaration = t.importDeclaration([importDefaultSpecifier], t.stringLiteral(pathToImport));
+          path.insertBefore(importDeclaration);
+        });
+
+        path.remove();
       }
     }
   };
--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
 import resolver from './resolver';
 
 export default ({ types: t }) => {
-  const importVisitors = {
-    ImportDeclaration(path, state) {
-      const lib = state.opts.lib;
-      const libNode = path.node.source
-      if(!(t.isStringLiteral(libNode) && libNode.value === lib)) {
-        return;
-      }
-      path.get('specifiers').forEach((spec) => {
-        const localName = spec.get('local').node.name;
-        const name = spec.get('imported').node.name;
-        const identifier = t.identifier(localName);
-        const importDefaultSpecifier = t.importDefaultSpecifier(identifier);
-        const pathToImport = resolver(lib, name);
-        const importDeclaration = t.importDeclaration([importDefaultSpecifier], t.stringLiteral(pathToImport));
-        path.insertBefore(importDeclaration);
-      });
-
-      path.remove();
-    },
-  };
-
   return {
     visitor: {
-      Program: {
-        exit(programPath, state) {
-          programPath.traverse(importVisitors, state);
-        },
+      ImportDeclaration(path, state) {
+        const lib = state.opts.lib;
+        const libNode = path.node.source
+        if (!(t.isStringLiteral(libNode) && libNode.value === lib)) {
+          return;
+        }
+        path.get('specifiers').forEach((spec) => {
+          const localName = spec.get('local').node.name;
+          const name = spec.get('imported').node.name;
+          const identifier = t.identifier(localName);
+          const importDefaultSpecifier = t.importDefaultSpecifier(identifier);
+          const pathToImport = resolver(lib, name);
+          const importDeclaration = t.importDeclaration([importDefaultSpecifier], t.stringLiteral(pathToImport));
+          path.insertBefore(importDeclaration);
+        });
+
+        path.remove();
       },
     },
   };
